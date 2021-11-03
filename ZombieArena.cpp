6342d68a34b137f85e557473c04302e8084895c0
emrecancorapci <emrecancorapci@gmail.com>
2021-11-03 15:56:05 +0300

    Some changes made
    Header file added
    Background tile added
    Character movement added

--- ZombieArena.cpp
+++ ZombieArena.cpp
@@ -1,8 +1,63 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "Player.h"
+#include "ZombieArena.h"
 
 using namespace sf;
+
+int createBackground(VertexArray& vertexArray, IntRect arena)
+{
+	// Size of each tile/texture
+	const int TILE_SIZE = 50;
+	const int TILE_TYPES = 3;
+	const int VERTS_IN_QUAD = 4;
+
+	int worldWidth = arena.width / TILE_SIZE;
+	int worldHeight = arena.height / TILE_SIZE;
+
+	vertexArray.setPrimitiveType(Quads);
+
+	// Size of the vertex array
+	vertexArray.resize(worldWidth * worldHeight * VERTS_IN_QUAD);
+
+	int currentVertex = 0;
+
+	for (int i = 0; i < worldWidth; i++)
+	{
+		for(int j = 0; j < worldHeight; j++)
+		{
+			vertexArray[currentVertex + 0].position = Vector2f(i * TILE_SIZE,j * TILE_SIZE);
+			vertexArray[currentVertex + 1].position = Vector2f((i + 1) * TILE_SIZE, j * TILE_SIZE);
+			vertexArray[currentVertex + 2].position = Vector2f((i + 1) * TILE_SIZE, (j + 1) * TILE_SIZE);
+			vertexArray[currentVertex + 3].position = Vector2f(i * TILE_SIZE, (j + 1) * TILE_SIZE);
+
+			if(i == (0 || worldWidth - 1) || j == (0 || worldHeight - 1))
+			{
+				vertexArray[currentVertex + 0].texCoords = Vector2f(0, TILE_TYPES * TILE_SIZE);
+				vertexArray[currentVertex + 1].texCoords = Vector2f(TILE_SIZE, TILE_TYPES * TILE_SIZE);
+				vertexArray[currentVertex + 2].texCoords = Vector2f(TILE_SIZE, (TILE_TYPES +1) * TILE_SIZE);
+				vertexArray[currentVertex + 3].texCoords = Vector2f(0, (TILE_TYPES +1) * TILE_SIZE);
+			}
+			else
+			{
+				srand((int)time(0) + j * (i-1));
+				int randomTile = rand() % TILE_TYPES;
+				int verticalOffset = randomTile * TILE_SIZE;
+
+				vertexArray[currentVertex + 0].texCoords = Vector2f(0, verticalOffset);
+				vertexArray[currentVertex + 1].texCoords = Vector2f(TILE_SIZE, verticalOffset);
+				vertexArray[currentVertex + 2].texCoords = Vector2f(TILE_SIZE, verticalOffset + TILE_SIZE);
+				vertexArray[currentVertex + 3].texCoords = Vector2f(0, verticalOffset + TILE_SIZE);
+
+			}
+
+			currentVertex = currentVertex + VERTS_IN_QUAD;
+		}
+	}
+
+	return TILE_SIZE;
+}
+
 
 int main()
 {
@@ -32,6 +87,10 @@ int main()
 	Player player;
 
 	IntRect arena;
+
+	VertexArray background;
+	Texture backgroundTexture;
+	backgroundTexture.loadFromFile("graphics/background_sheet.png");
 
 #pragma endregion
 
@@ -98,8 +157,7 @@ int main()
 				arena.left = 0;
 				arena.top = 0;
 
-				// Will be modified
-				int tileSize = 50;
+				int tileSize = createBackground(background, arena);
 
 				player.spawn(arena,resolution, tileSize);
 
@@ -137,6 +195,8 @@ int main()
 			std::cout << "playing" << std::endl;;
 			window.clear();
 
+			window.draw(background, &backgroundTexture);
+
 			window.setView(mainView);
 			window.draw(player.getSprirte());
 		}
@@ -155,7 +215,7 @@ int main()
 
 		if (state == State::GAME_OVER)
 		{
-			std::cout << "gameover" << std::endl;;
+			std::cout << "game-over" << std::endl;;
 		}
 
 		window.display();
---
+++ ZombieArena.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+int createBackground(VertexArray& vertexArray, IntRect arena);
